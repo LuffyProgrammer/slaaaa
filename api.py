38ad5270009d58fa8fc669f358f729362167ea2c
Luiz Eduardo <49507325+RuisuEduardo@users.noreply.github.com>
2021-11-23 01:14:44 -0300

    Add files via upload

---
+++ api.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from threading import Thread
+import random
+
+app = Flask('')
+
+@app.route('/')
+def home():
+	return '''
+{
+    "status": "200"
+    "message": "NÃO TEM NADA AQUI PORRA!"
+}'''
+
+@app.route('/teste')
+def api1():
+	return '''
+{
+    "status": "200"
+    "message": "Clarisse, me chupa kkkkkkkkkkh"
+}'''
+
+
+def run():
+	app.run(host='0.0.0.0', port=random.randint(2000, 9000))
+
+def keep_alive():
+
+	t = Thread(target=run)
+	t.start()
---
+++ start.py
@@ -0,0 +1,8 @@
+from api import keep_alive
+import pyfiglet
+luffy = 'Criado por Luffy Programmer'
+print(pyfiglet.figlet_format("Server\nStarter", font="slant"))
+print(luffy)
+keep_alive()
+
+print("Starting your server...");
