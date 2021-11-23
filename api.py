0616234be898e0421abe48f3b21eef2fe5fc6955
Luiz Eduardo <49507325+RuisuEduardo@users.noreply.github.com>
2021-11-23 01:20:06 -0300

    Update api.py

--- api.py
+++ api.py
@@ -22,7 +22,7 @@ def api1():
 
 
 def run():
-	app.run(host='0.0.0.0', port=random.randint(2000, 9000))
+	app.run(host='https://ssserverapi.herokuapp.com/', port=random.randint(2000, 9000))
 
 def keep_alive():
 
