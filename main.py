d4c9ff6cfbb5068f48c323acd9709e818ab302e1
Luiz Eduardo <49507325+RuisuEduardo@users.noreply.github.com>
2021-11-23 01:31:03 -0300

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from api import keep_alive
-import pyfiglet
-luffy = 'Criado por Luffy Programmer'
-print(pyfiglet.figlet_format("Server\nStarter", font="slant"))
-print(luffy)
-keep_alive()
-
-print("Starting your server...");
