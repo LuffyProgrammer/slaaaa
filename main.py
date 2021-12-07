345544db94ebe2058ef61299fe0ebec86531980e
Luiz Eduardo <49507325+RuisuEduardo@users.noreply.github.com>
2021-12-07 02:57:06 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 
 while True:
-    os.system('pip install colorama && pip install json && pip install pyfiglet && pip install requests')
+ #   os.system('pip install colorama && pip install json && pip install pyfiglet && pip install requests')
     os.system('python3 starter.py')
     os.system('python3 coin.py')
     os.system('python3 coin2.py')
